3118e987c392e5bd37b4f566d07c2cbee63b52f1
wenzhemint <wenzhemint@outlook.com>
2024-04-28 02:19:24 +0800

    create Switch button for switching between themes.

--- src/components/MenuComp/MenuComp.tsx
+++ src/components/MenuComp/MenuComp.tsx
@@ -6,38 +6,45 @@ import { HomeOutlined, InfoCircleOutlined } from '@ant-design/icons'
 import { useNavigate } from "react-router-dom"
 import { useSelector, useDispatch } from "react-redux"
 import { updateMenuTab } from "../../redux/menu/menuSlice"
+import { Switch } from "antd"
 
 const items: MenuProps['items'] = [
-    {
-      label: 'Main Page',
-      key: 'main',
-      icon: <HomeOutlined />,
-    },
-    {
-      label: 'About Page',
-      key: 'about',
-      icon: <InfoCircleOutlined />
-    }
-  ];
+  {
+    label: 'Main Page',
+    key: 'main',
+    icon: <HomeOutlined />,
+  },
+  {
+    label: 'About Page',
+    key: 'about',
+    icon: <InfoCircleOutlined />
+  }
+];
 
 const MenuComp: FC = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate()
-  const [current, setCurrent] = useState('mail')
   const selectedTab = useSelector((state: any) => state.menu.selectedTab)
 
   const onSwichTab: MenuProps['onClick'] = (e) => {
-      console.log('menu clicked ', e);
-      const menuKey = e?.key || '/'
-      setCurrent(menuKey);
-      dispatch(updateMenuTab(menuKey))
-      navigate('/'+menuKey)
+    console.log('menu clicked ', e);
+    const menuKey = e?.key || '/'
+    dispatch(updateMenuTab(menuKey))
+    navigate('/'+menuKey)
+  };
+
+  const onChangeTheme = (checked: boolean) => {
+    console.log(`switch to ${checked}`);
   };
 
   return (
-      <div className="menu-comp">
-          <Menu onClick={onSwichTab} selectedKeys={[selectedTab]} mode="horizontal" items={items} />
+    <div className={`${styles.menuComp}`}>
+      <Menu onClick={onSwichTab} selectedKeys={[selectedTab]} mode="horizontal" items={items} theme="dark" />
+
+      <div className={`${styles.swichMode}`}>
+        <Switch onChange={onChangeTheme} defaultChecked={true} />
       </div>
+    </div>
   );
 };
 
