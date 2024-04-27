dc9b293aaeb78ef311efa0e67af171b908cf3849
wenzhemint <wenzhemint@outlook.com>
2024-04-28 00:08:09 +0800

    set router and create Main page.

---
+++ src/pages/MainPage/MainPage.tsx
@@ -0,0 +1,11 @@
+import { FC } from "react";
+
+const MainPage: FC = () => {
+    return (
+        <div>
+            Main Page
+        </div>
+    );
+};
+
+export default MainPage;
---
+++ src/router/router.tsx
@@ -0,0 +1,8 @@
+import MainPage from '../pages/MainPage/MainPage';
+
+export const routerConfig = [
+    {
+        path: "/",
+        element: <MainPage />,
+    }
+];
